5d3d307127168870d48cdfbcf930a2213ec1adaf
Roxy <Roxy@DESKTOP-336JRR4>
2016-10-10 13:49:28 +0200

    Add project files.

---
+++ 12_Employee/Employee.cs
@@ -0,0 +1,26 @@
+﻿namespace _12_Employee
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public string Type { get; internal set; }
+        public long Id { get; internal set; }
+
+ 
+
+
+       
+        public Employee(string Name, string Type, long Id)
+
+        {
+            this.Name = Name;
+            this.Type = Type;
+            this.Id = Id;
+
+        }
+
+        public Employee()
+        {
+        }
+    }
+}
---
+++ 12_Employee/EmployeeRepository.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _12_Employee
+{
+    internal class EmployeeRepository
+    {
+
+        Dictionary<long, Employee> emp = new Dictionary<long, Employee>();
+
+        private static long id = 0;
+
+        internal void Clear()
+        {
+            emp.Clear();
+        }
+
+        internal long CountEmployees()
+        {
+            return emp.Count;
+        }
+
+        internal Employee CreateEmployee(string v1, string v2)
+        {
+            id++;
+            Employee NewEmployee = new Employee();
+            NewEmployee.Name = v1;
+            NewEmployee.Type = v2;
+            NewEmployee.Id = id;
+            return NewEmployee;
+
+        }
+
+        internal void SaveEmployee(Employee employee)
+        {
+            if (emp.ContainsKey(employee.Id))
+                emp[employee.Id] = employee;
+            else
+                emp.Add(employee.Id, employee);
+                
+        }
+
+        internal Employee LoadEmployee(long id)
+        {
+            return emp[id];
+        }
+
+        internal List<Employee> FindAllEmployees()
+        {
+            List<Employee> returnList = new List<Employee>();
+            foreach (KeyValuePair<long, Employee> entry in emp)
+            {
+                returnList.Add(entry.Value);
+
+            }
+            return returnList;
+        }
+    }
+}
